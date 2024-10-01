bcf15cdaf5a392ecb3ac44e5e4b83cb0209eb2fc
Sadiq Salau <sadiqsalau888@gmail.com>
2024-10-01 12:41:05 +0100

    [UPDATE] Show all IPV4 addresses

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const chalk = require("chalk");
 const http = require("http");
 const { Server } = require("socket.io");
+const { networkInterfaces } = require("os");
 
 const PORT = 7777;
 const server = http.createServer();
@@ -18,10 +19,33 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(
-    `${chalk.green("Server is up and running on")} ${chalk.bold.yellow(
-      `http://localhost:${PORT}`
-    )}`
-  );
-});
+server
+  .listen(PORT, () => {
+    const nets = networkInterfaces();
+
+    console.log(chalk.bold.green("Sync Server is up and running \n"));
+
+    for (const interfaces of Object.values(nets)) {
+      for (const net of interfaces) {
+        const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
+
+        if (net.family === familyV4Value) {
+          /** Show Address */
+          console.log(
+            `${chalk.blue("[ADDR]")} ${chalk.bold.yellow(
+              `${net.address}:${PORT}`
+            )}`
+          );
+        }
+      }
+    }
+  })
+  .on("error", () => {
+    console.log(chalk.bold.red("Failed to Start Sync Server"));
+
+    console.log(
+      chalk.yellow(
+        "Please ensure the server isn't running already. You should restart your computer if the problem persists. \n"
+      )
+    );
+  });
